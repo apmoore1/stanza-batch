7af84ec20c6f8de047f73e9181dcb33c2bbee140
Andrew Moore <andrew.p.moore94@gmail.com>
2021-02-04 17:12:25 +0000

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="stanza_batch",
-    version="0.1.0",
+    version="0.1.1",
     description="A batching utility for Stanza",
     long_description=long_description,
     long_description_content_type="text/markdown",
