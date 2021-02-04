e375b0b34650048a62f991edbbd9b14de1eaf4ef
Andrew Moore <andrew.p.moore94@gmail.com>
2021-02-04 16:48:26 +0000

    Formatting with black

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8"
+        "Programming Language :: Python :: 3.8",
     ],
     # Came from the Stanza setup.py
     keywords="natural-language-processing nlp natural-language-understanding stanford-nlp deep-learning",
